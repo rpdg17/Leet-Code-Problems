ed0efea7d893f00fbf83e7390bdd108afdacabbf
rpdg17 <robindeguzman17@gmail.com>
2022-08-22 13:21:36 +0800

    Two Sum

---
+++ TwoSum.php
@@ -0,0 +1,28 @@
+<?php
+
+// LeetCode Problem : Two Sum
+// Around 120ms
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function twoSum($nums, $target) {
+        $numberOfArrays = count($nums);
+        
+        for($x=0; $x < $numberOfArrays; $x++){
+            $diffNum = $target - $nums[$x];
+            
+            unset($nums[$x]);
+            if(array_search($diffNum, $nums) !== false){
+                $indexTwo = array_search($diffNum, $nums);
+                
+                return [$x, $indexTwo];
+            }
+        }
+    }
+}
+?>
