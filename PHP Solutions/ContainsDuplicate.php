b20ffa3f7472e8bb677c1b99aaa5d32bd7460395
rpdg17 <robindeguzman17@gmail.com>
2022-08-25 08:12:22 +0800

    Contains Duplicate

---
+++ PHP Solutions/ContainsDuplicate.php
@@ -0,0 +1,19 @@
+<?php
+
+// LeetCode Problem: Constain Duplicate
+// Around 35ms
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Boolean
+     */
+    function containsDuplicate($nums) {
+        $result = array_unique($nums);
+        
+        return sizeof($nums) == sizeof($result) ? false : true;
+        
+    }
+}
+?>
