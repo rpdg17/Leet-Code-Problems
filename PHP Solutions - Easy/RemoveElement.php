4b1b100694d542cd77819e71e7e08c34e90a9d55
rpdg17 <robindeguzman17@gmail.com>
2022-08-26 08:07:08 +0800

    Remove Element

---
+++ PHP Solutions - Easy/RemoveElement.php
@@ -0,0 +1,26 @@
+<?php
+
+// LeetCode Problem: Remove Element
+// Around 10ms
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $val
+     * @return Integer
+     */
+    function removeElement(&$nums, $val) {
+        $k = 0;
+        for ($x = 0; $x < sizeof($nums); $x++){
+            if($nums[$x] != $val) {
+                $nums[$k] = $nums[$x];
+                $k++;
+            }
+        }
+        
+        return $k;
+    }
+}
+
+?>
