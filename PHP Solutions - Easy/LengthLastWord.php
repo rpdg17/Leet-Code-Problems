3682bc580155c6296f9f9633b7e791b57812f700
rpdg17 <robindeguzman17@gmail.com>
2022-08-26 08:05:45 +0800

    Length of Last Word

---
+++ PHP Solutions - Easy/LengthLastWord.php
@@ -0,0 +1,21 @@
+<?php
+
+// LeetCode Problem: Length of Last Word
+// Around 11ms
+
+class Solution {
+
+    /**
+     * @param String $s
+     * @return Integer
+     */
+    function lengthOfLastWord($s) {
+        $splitted = preg_split("/[^\w]*([\s]+[^\w]*|$)/", $s, -1, PREG_SPLIT_NO_EMPTY);
+        
+        $splittedCount = sizeof($splitted);
+        
+        return strlen($splitted[$splittedCount - 1]);
+    }
+}
+
+?>
